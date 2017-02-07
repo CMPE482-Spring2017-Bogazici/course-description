a48e1f71f2fab8fd72b814351ac60face85e4300
A. Taylan Cemgil <taylan.cemgil@boun.edu.tr>
2017-02-07 17:41:28 +0200

    jemdoc.bat modified

--- jemall.bat
+++ jemall.bat
@@ -1,5 +1,3 @@
 python jemdoc.py -o index.html index_cmpe482.jemdoc
-
-cp *.html ~/Dropbox/Public/cmpe482
-cp *.html /Volumes/taylan.cemgil/public_html/Courses/cmpe482
-cp -R eqs /Volumes/taylan.cemgil/public_html/Courses/cmpe482
+rsync -va ~/src/Courses/cmpe482/* ~/Dropbox/Public/cmpe482/
+rsync -va ~/src/Courses/cmpe482/* /Volumes/taylan.cemgil/public_html/Courses/cmpe482/

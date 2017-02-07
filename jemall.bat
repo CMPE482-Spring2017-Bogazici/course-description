4857e52f511217ef4ea9c7e2c957174712201af2
A. Taylan Cemgil <taylan.cemgil@boun.edu.tr>
2017-02-07 14:02:29 +0200

    Initial 2017 version

---
+++ jemall.bat
@@ -0,0 +1,5 @@
+python jemdoc.py -o index.html index_cmpe482.jemdoc
+
+cp *.html ~/Dropbox/Public/cmpe482
+cp *.html /Volumes/taylan.cemgil/public_html/Courses/cmpe482
+cp -R eqs /Volumes/taylan.cemgil/public_html/Courses/cmpe482
